9add851bf0addb5b08f3fb66a4e9a0c531acb159
Adrian Pawlak <bebeto9009@gmail.com>
2018-01-20 20:14:12 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', function () {
+    var files = [
+        'public/*.html',
+        'src/css/*.scss',
+        'src/js/*.js'
+    ];
+
+    browserSync.init(files, {
+        server: {
+            baseDir: 'public'
+        }
+    });
+});
+
+// Default task to be run with `gulp`
+gulp.task('default', ["browser-sync", "sass", "watch"]);
+
+gulp.task('sass', function(){
+    return gulp.src('src/css/styles.scss')
+        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(gulp.dest('public/assets/css/'))
+});
+gulp.task('watch', function(){
+    gulp.watch('src/css/styles.scss', ['sass']);
+
+    // Other watchers
+});
+
+gulp.watch("public/*.html").on('change', browserSync.reload);
+
