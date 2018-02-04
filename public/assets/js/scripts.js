042bf26a62314f81a2791f248e3c51ab925fb650
Adrian Pawlak <bebeto9009@gmail.com>
2018-02-05 00:32:43 +0100

    some try with menu

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,5 +1,64 @@
-$(document).ready(function(){
-    $(".hamburger").click(function(){
-        $(this).toggleClass("is-active");
-    });
-});
+/* menu toggle */
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+function toggleMenu() {
+    document.getElementsByClassName('menu__nav is-open').classList.remove('is-open');
+}
+
+
+
+// document.getElementsByClassName('menu__nav-bar').addEventListener('clock', function() {
+//     document.getElementsByClassName('menu__nav')[0].classList.toggle('change');
+// });
+
+// document.getElementsByClassName( 'menu__nav-bar' ).addEventListener( 'click', function () {
+//
+//     document.getElementsByClassName( 'menu__nav' ).toggleClass( 'hide' );
+//
+// }, false );
+//
+// Element.prototype.toggleClass = function ( className ) {
+//
+//     if( this.className.split( ' ' ).indexOf( className ) == -1 ) {
+//
+//         this.className = ( this.className + ' ' + className ).trim();
+//
+//     } else {
+//
+//         this.className = this.className.replace( new RegExp( '(\\s|^)' + className + '(\\s|$)' ), ' ' ).trim();
+//
+//     };
+//
+// };
+//
+
+ // let rwd = document.getElementsByClassName('menu__nav-bar');
+ //
+ // rwd.addEventListener('click', function(e) {
+ //     e.remove('is-open') {
+ //         else {
+ //             e.add('is-open');
+ //         }
+ //     }
+ // })
+
+// let rwd = document.getElementsByClassName('menu__nav-bar');
+//
+// rwd.addEventListener('click', function(){
+//
+// });
+
+// myFunction () {
+//     document.getElementsByClassName('menu__nav-bar').classList.toggle('change');
+// };
+//
+// document.getElementsByClassName('menu__nav-bar').addEventListener('click', function (){
+//    if (this.classList.contains('change')) {
+//        this.classList.remove('change');
+//
+//    }else{
+//        this.classList.add('change');
+//    }
+// });
