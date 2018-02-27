4929842a3ad3fc4088d5e989e7f2493bc7ae2418
Adrian <adrian@netkata.com>
2018-02-27 10:00:14 +0100

    final ver 0.0.2

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -24,20 +24,4 @@ var links = document.querySelectorAll('ul.menu__nav li a');
 for (var i = 0; i < links.length; i++) {
     links[i].addEventListener('click', handleLinkClick);
 }
-
 /* end smooth scroll */
-
-// Get the container element
-var btnContainer = document.getElementsByClassName("menu__nav");
-
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.querySelectorAll('link');
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
